c240238e399922413c2dcb58ab60def4eabbb6ea
theperfectfuel <robert@theperfectfuel.com>
2016-01-24 00:23:18 -0800

    Completed layout with bourbon, neat and some bitters.

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
-	
+
+	function getSearch(query) {
+		URL='http://www.omdbapi.com/?s=hockey&r=json';
+		//URL='http://nhlwc.cdnak.neulion.com/fs1/nhl/league/playerstatsline/20152016/2/' + query + '/iphone/playerstatsline.json';
+		getMovieData(URL);
+	}
+
+	function getMovieData(URL) {
+		$.getJSON(URL, function(data) {
+				myData = data.Search;
+				showMovieData(myData);
+		});
+		
+	}
+
+	function clearMovieData() {
+		$("p").remove();
+	}
+
+	function showMovieData(myData) {
+		for (var x in myData) {
+			$("#search-results")
+			.append("<p>" + myData[x].Title + "</p>");
+			console.log(myData[x].Title);
+		}
+	}
+
+	$(this).ready(function(event) {
+		var query = $("#query").val();
+		getSearch(query);
+		event.preventDefault();
+	});
+
+
 });
